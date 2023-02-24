bcc7c65c12335c99fe32b5d03f49467116fcbc5a
1ucky40nc3 <louisnwendler@gmail.com>
2023-02-24 21:33:48 +0100

    reformatted code

--- src/h3/models/ssm_seq.py
+++ src/h3/models/ssm_seq.py
@@ -33,42 +33,81 @@ CausalLMOutput = namedtuple('CausalLMOutput', ['logits', 'hidden_states'])
 def create_mixer_cls(ssm_cls=H3, ssm_cfg=None, attn_layer_idx=None, attn_cfg=None, layer_idx=None):
     if attn_layer_idx is not None and layer_idx in attn_layer_idx:
         causal = True if attn_cfg is None else attn_cfg.pop('causal', True)
-        mixer_cls = partial(MHA, layer_idx=layer_idx, causal=causal, 
-                            **(attn_cfg if attn_cfg is not None else {}))
+        mixer_cls = partial(
+            MHA, 
+            layer_idx=layer_idx, 
+            causal=causal, 
+            **(attn_cfg if attn_cfg is not None else {})
+        )
     else:
-        mixer_cls = partial(ssm_cls, layer_idx=layer_idx,
-                            **(ssm_cfg if ssm_cfg is not None else {}))
+        mixer_cls = partial(
+            ssm_cls, 
+            layer_idx=layer_idx,
+            **(ssm_cfg if ssm_cfg is not None else {})
+        )
     return mixer_cls
 
 
 def create_mlp_cls(d_model, d_inner=None, fused_mlp=False):
     inner_dim = d_inner if d_inner is not None else 4 * d_model
     if not fused_mlp:
-        mlp_cls = partial(Mlp, hidden_features=inner_dim,
-                          activation=partial(F.gelu, approximate='tanh'))
+        mlp_cls = partial(
+            Mlp, 
+            hidden_features=inner_dim,
+            activation=partial(F.gelu, approximate='tanh')
+        )
     else:
         mlp_cls = partial(FusedMLP, hidden_features=inner_dim)
     return mlp_cls
 
 
-def create_block(d_model, d_inner=None, ssm_cls=H3, ssm_cfg=None, attn_layer_idx=None,
-                 attn_cfg=None, layer_norm_epsilon=1e-5,
-                 resid_dropout1=0.0, resid_dropout2=0.0, residual_in_fp32=False,
-                 fused_mlp=False, fused_dropout_add_ln=False, layer_idx=None):
-    mixer_cls = create_mixer_cls(ssm_cls=ssm_cls, ssm_cfg=ssm_cfg, attn_layer_idx=attn_layer_idx,
-                                 attn_cfg=attn_cfg, layer_idx=layer_idx)
+def create_block(
+    d_model, 
+    d_inner=None, 
+    ssm_cls=H3, 
+    ssm_cfg=None, 
+    attn_layer_idx=None,
+    attn_cfg=None, 
+    layer_norm_epsilon=1e-5,
+    resid_dropout1=0.0, 
+    resid_dropout2=0.0, 
+    residual_in_fp32=False,
+    fused_mlp=False, 
+    fused_dropout_add_ln=False, 
+    layer_idx=None
+):
+    mixer_cls = create_mixer_cls(
+        ssm_cls=ssm_cls, 
+        ssm_cfg=ssm_cfg, 
+        attn_layer_idx=attn_layer_idx,
+        attn_cfg=attn_cfg, 
+        layer_idx=layer_idx
+    )
     mlp_cls = create_mlp_cls(d_model, d_inner=d_inner, fused_mlp=fused_mlp)
     norm_cls = partial(nn.LayerNorm, eps=layer_norm_epsilon)
-    block = Block(d_model, mixer_cls, mlp_cls, norm_cls=norm_cls,
-                  prenorm=True, resid_dropout1=resid_dropout1, resid_dropout2=resid_dropout2,
-                  fused_dropout_add_ln=fused_dropout_add_ln, residual_in_fp32=residual_in_fp32)
+    block = Block(
+        d_model, 
+        mixer_cls, 
+        mlp_cls, 
+        norm_cls=norm_cls,
+        prenorm=True, 
+        resid_dropout1=resid_dropout1, 
+        resid_dropout2=resid_dropout2,
+        fused_dropout_add_ln=fused_dropout_add_ln, 
+        residual_in_fp32=residual_in_fp32
+    )
     block.layer_idx = layer_idx
     return block
 
 
 # https://github.com/huggingface/transformers/blob/c28d04e9e252a1a099944e325685f14d242ecdcd/src/transformers/models/gpt2/modeling_gpt2.py#L454
-def _init_weights(module, n_layer, initializer_range=0.02, rescale_prenorm_residual=True,
-                  glu_act=False):
+def _init_weights(
+    module, 
+    n_layer, 
+    initializer_range=0.02, 
+    rescale_prenorm_residual=True,
+    glu_act=False
+):
     if isinstance(module, nn.Linear):
         nn.init.normal_(module.weight, std=initializer_range)
         if module.bias is not None:
@@ -100,13 +139,26 @@ def _init_weights(module, n_layer, initializer_range=0.02, rescale_prenorm_resid
 
 
 class SSMModel(nn.Module):
-
-    def __init__(self, d_model: int, n_layer: int, d_inner: int, vocab_size: int, ssm_cfg=None,
-                 attn_layer_idx=None, attn_cfg=None, max_position_embeddings=0,
-                 resid_dropout: float = 0.0, embed_dropout: float = 0.1, dropout_cls=nn.Dropout,
-                 layer_norm_epsilon: float = 1e-5, initializer_cfg=None,
-                 fused_mlp=False, fused_dropout_add_ln=False, residual_in_fp32=False,
-                 **kwargs) -> None:
+    def __init__(
+        self, 
+        d_model: int, 
+        n_layer: int, 
+        d_inner: int, 
+        vocab_size: int, 
+        ssm_cfg=None,
+        attn_layer_idx=None, 
+        attn_cfg=None, 
+        max_position_embeddings=0,
+        resid_dropout: float = 0.0, 
+        embed_dropout: float = 0.1, 
+        dropout_cls=nn.Dropout,
+        layer_norm_epsilon: float = 1e-5, 
+        initializer_cfg=None,
+        fused_mlp=False, 
+        fused_dropout_add_ln=False, 
+        residual_in_fp32=False,
+        **kwargs
+    ) -> None:
         super().__init__()
         self.embeddings = GPT2Embeddings(d_model, vocab_size, max_position_embeddings)
         self.residual_in_fp32 = residual_in_fp32
@@ -121,19 +173,33 @@ class SSMModel(nn.Module):
         if self.fused_dropout_add_ln and dropout_add_layer_norm is None:
             raise ImportError('dropout_add_layer_norm is not installed')
 
-        self.layers = nn.ModuleList([create_block(
-            d_model, d_inner=d_inner, ssm_cfg=ssm_cfg, attn_layer_idx=attn_layer_idx,
-            attn_cfg=attn_cfg, layer_norm_epsilon=layer_norm_epsilon,
-            resid_dropout1=embed_dropout if i == 0 else resid_dropout,
-            resid_dropout2=resid_dropout, residual_in_fp32=residual_in_fp32,
-            fused_mlp=fused_mlp, fused_dropout_add_ln=fused_dropout_add_ln, layer_idx=i
-        ) for i in range(n_layer)])
+        self.layers = nn.ModuleList([
+            create_block(
+                d_model, 
+                d_inner=d_inner, 
+                ssm_cfg=ssm_cfg, 
+                attn_layer_idx=attn_layer_idx,
+                attn_cfg=attn_cfg, 
+                layer_norm_epsilon=layer_norm_epsilon,
+                resid_dropout1=embed_dropout if i == 0 else resid_dropout,
+                resid_dropout2=resid_dropout, 
+                residual_in_fp32=residual_in_fp32,
+                fused_mlp=fused_mlp, 
+                fused_dropout_add_ln=fused_dropout_add_ln, 
+                layer_idx=i
+            ) for i in range(n_layer)
+        ])
 
         self.drop_f = nn.Dropout(resid_dropout)
         self.ln_f = nn.LayerNorm(d_model, eps=layer_norm_epsilon)
 
-        self.apply(partial(_init_weights, n_layer=n_layer,
-                           **(initializer_cfg if initializer_cfg is not None else {})))
+        self.apply(
+            partial(
+                _init_weights, 
+                n_layer=n_layer,
+                **(initializer_cfg if initializer_cfg is not None else {})
+            )
+        )
 
     def forward(self, input_ids, position_ids=None, inference_params=None):
         hidden_states = self.embeddings(input_ids, position_ids=position_ids)
@@ -150,45 +216,82 @@ class SSMModel(nn.Module):
         else:
             # Set prenorm=False here since we don't need the residual
             hidden_states = dropout_add_layer_norm(
-                hidden_states, residual, self.ln_f.weight, self.ln_f.bias,
-                self.drop_f.p if self.training else 0.0, self.ln_f.eps, prenorm=False,
+                hidden_states, 
+                residual, 
+                self.ln_f.weight, 
+                self.ln_f.bias,
+                self.drop_f.p if self.training else 0.0, 
+                self.ln_f.eps, 
+                prenorm=False,
                 residual_in_fp32=self.residual_in_fp32
             )
         return hidden_states
 
 
 class SSMLMHeadModel(nn.Module, GenerationMixin):
-
-    def __init__(self, d_model: int, n_layer: int, d_inner: int, vocab_size: int, ssm_cfg=None,
-                 attn_layer_idx=None, attn_cfg=None, max_position_embeddings=0,
-                 resid_dropout: float = 0.0, embed_dropout: float = 0.1, dropout_cls=nn.Dropout,
-                 layer_norm_epsilon: float = 1e-5, initializer_cfg=None,
-                 fused_mlp=False, fused_dropout_add_ln=False, residual_in_fp32=False,
-                 pad_vocab_size_multiple: int = 1, **kwargs) -> None:
+    def __init__(
+        self, 
+        d_model: int, 
+        n_layer: int, 
+        d_inner: int, 
+        vocab_size: int, 
+        ssm_cfg=None,
+        attn_layer_idx=None, 
+        attn_cfg=None, 
+        max_position_embeddings=0,
+        resid_dropout: float = 0.0, 
+        embed_dropout: float = 0.1, 
+        dropout_cls=nn.Dropout,
+        layer_norm_epsilon: float = 1e-5, 
+        initializer_cfg=None,
+        fused_mlp=False, 
+        fused_dropout_add_ln=False, 
+        residual_in_fp32=False,
+        pad_vocab_size_multiple: int = 1, 
+        **kwargs
+    ) -> None:
         super().__init__()
         if vocab_size % pad_vocab_size_multiple != 0:
             vocab_size += pad_vocab_size_multiple - (vocab_size % pad_vocab_size_multiple)
         self.backbone = SSMModel(
-            d_model=d_model, n_layer=n_layer, d_inner=d_inner, vocab_size=vocab_size,
-            ssm_cfg=ssm_cfg, attn_layer_idx=attn_layer_idx, attn_cfg=attn_cfg,
+            d_model=d_model, 
+            n_layer=n_layer, 
+            d_inner=d_inner, 
+            vocab_size=vocab_size,
+            ssm_cfg=ssm_cfg, 
+            attn_layer_idx=attn_layer_idx, 
+            attn_cfg=attn_cfg,
             max_position_embeddings=max_position_embeddings,
-            resid_dropout=resid_dropout, embed_dropout=embed_dropout,
-            dropout_cls=dropout_cls, layer_norm_epsilon=layer_norm_epsilon,
-            initializer_cfg=initializer_cfg, fused_mlp=fused_mlp,
-            fused_dropout_add_ln=fused_dropout_add_ln, residual_in_fp32=residual_in_fp32, **kwargs
+            resid_dropout=resid_dropout, 
+            embed_dropout=embed_dropout,
+            dropout_cls=dropout_cls, 
+            layer_norm_epsilon=layer_norm_epsilon,
+            initializer_cfg=initializer_cfg, 
+            fused_mlp=fused_mlp,
+            fused_dropout_add_ln=fused_dropout_add_ln, 
+            residual_in_fp32=residual_in_fp32, 
+            **kwargs
         )
         self.lm_head = nn.Linear(d_model, vocab_size, bias=False)
         # Initialize weights and apply final processing
-        self.apply(partial(_init_weights, n_layer=n_layer,
-                           **(initializer_cfg if initializer_cfg is not None else {})))
+        self.apply(
+            partial(
+                _init_weights, 
+                n_layer=n_layer,
+                **(initializer_cfg if initializer_cfg is not None else {})
+            )
+        )
         self.tie_weights()
 
     def tie_weights(self):
         self.lm_head.weight = self.backbone.embeddings.word_embeddings.weight
 
     def forward(self, input_ids, position_ids=None, inference_params=None):
-        hidden_states = self.backbone(input_ids, position_ids=position_ids,
-                                      inference_params=inference_params)
+        hidden_states = self.backbone(
+            input_ids, 
+            position_ids=position_ids,
+            inference_params=inference_params
+        )
         lm_logits = self.lm_head(hidden_states)
         
         return CausalLMOutput(logits=lm_logits, hidden_states=hidden_states)
